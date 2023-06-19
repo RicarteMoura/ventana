dab1c64d147b0aeb1cb22df0bf04a96f30d56fa8
RicarteMoura <137024108+RicarteMoura@users.noreply.github.com>
2023-06-19 19:13:33 -0300

    Update mail_config.php

--- mail_config.php
+++ mail_config.php
@@ -6,9 +6,9 @@ $mail->SMTPAuth = true; //Ativa email autenticado
 $mail->Host = 'smtp.gmail.com'; //Servidor de envio
 $mail->Port = '587'; //Porta de envio
 $mail->SMTPSecure = 'tls';
-$mail->Username = 'arquivo.ventana@ricarte.eti.br'; //email para smtp autenticado
-$mail->Password = 'Servs@DM'; //seleciona a porta de envio
-$mail->From = 'arquivo.ventana@ricarte.eti.br'; //remtente
+$mail->Username = 'website.com.br'; //email para smtp autenticado
+$mail->Password = 'Sdua Senha'; //seleciona a porta de envio
+$mail->From = 'aseuemail@gmail.comr'; //remtente
 $mail->FromName = 'Arquivo Ventana Serra'; //remtetene nome
 $mail->IsHTML(true);
 $mail->Subject = utf8_decode($assunto); //assunto
